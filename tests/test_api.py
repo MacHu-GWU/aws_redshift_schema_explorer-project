c5e3c382b5e1097a1f0cd0fda3ede056d3796113
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-10 17:03:02 -0400

    Add API enums and classes to test coverage
    Expanded the test to reference various enums and classes from the api module to ensure they are included in test coverage. Also fixed a typo in the import path for run_cov_test.

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,10 +5,27 @@ from aws_redshift_schema_explorer import api
 
 def test():
     _ = api
+    _ = api.SVV_REDSHIFT_DATABASES_DATABASE_TYPE_ENUM
+    _ = api.SVV_ALL_SCHEMAS_SCHEMA_TYPE_ENUM
+    _ = api.SVV_ALL_TABLES_TABLE_TYPE_ENUM
+    _ = api.SVV_TABLES_TABLE_TYPE_ENUM
+    _ = api.SVV_EXTERNAL_TABLES_TABLE_TYPE_ENUM
+    _ = api.SVV_TABLE_INFO_DISTSTYLE_ENUM
+    _ = api.SVV_ALL_COLUMNS_DATA_TYPE_ENUM
+    _ = api.PG_CLASS_REL_KIND_ENUM
+    _ = api.Database
+    _ = api.Schema
+    _ = api.Table
+    _ = api.View
+    _ = api.Column
+    _ = api.ListDatabaseCommand
+    _ = api.ListSchemaCommand
+    _ = api.ListTableCommand
+    _ = api.ListColumnsCommand
 
 
 if __name__ == "__main__":
-    from aws_redshift_schema_explorer.tests import run_cov_test
+    from aws_redshift_scehema_explorer.tests import run_cov_test
 
     run_cov_test(
         __file__,
